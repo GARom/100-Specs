8ea7fae6fc71f0ab127710346b9f780c1e428a66
Guillermo Romero <garomero1994@yahoo.com>
2015-02-03 18:26:27 -1000

    declare var beers

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -264,6 +264,20 @@ var scmList = ["git", "svn", "mercurial", "bazaar", "cvs"];
  *
  */
 
+var beers = {
+  IPA : "Ale",
+  Lager : "Strong",
+  Heffeweisen : "German",
+  Stout : [
+          "Thick",
+          "Dark"
+          ],
+  Porter : "Bitter",
+  Ale : [
+          "Light",
+          "Golden"
+          ]
+}
 
 /* Step 20
  *
